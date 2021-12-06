c7def0c42a6f5666c39f32db973eabd737446f03
issalafi <issa.lafi@gmail.com>
2021-12-06 18:51:19 +0200

    first Commit Styles + Dark Mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  darkTheme: boolean = false;
+
   title = 'Amazing-solutions';
+
+  constructor(private router: Router,
+              @Inject(PLATFORM_ID) private platformId: Object) {
+
+  }
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (localStorage.getItem('theme')) {
+        this.darkTheme = Boolean(Number(localStorage.getItem('theme')));
+      } else {
+        this.onChangeDarkMode();
+      }
+    }
+  }
+  onChangeDarkMode() {
+    if (this.darkTheme) {
+      localStorage.setItem('theme', '1')
+    } else {
+      localStorage.setItem('theme', '0')
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
